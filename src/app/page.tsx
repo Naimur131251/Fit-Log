9e03664bfaf8b2d41f59da52100f1d8e2005b671
Naimur131251 <naimur131251@gmail.com>
2026-09-23 23:36:38 +0600

    added to banner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Banner from "@/components/homepage/Banner";
 
 export default function Home() {
   return (
     <>
-      <h1 className="flex justify-center text-9xl text-amber-300">Fit Log</h1>
+      <Banner />
     </>
   );
 }
---
+++ src/components/homepage/Banner.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import BannerImage from "@/assets/banner.png";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const Banner = () => {
+  return (
+    <div className="container mx-auto">
+      <section className="flex items-center justify-between p-14 rounded-xl border border-[#222630] bg-[#15171D]">
+        {/* Left Content */}
+        <div className="space-y-5">
+          <p
+            className={`${inter.className} text-xs font-bold uppercase tracking-wider text-[#C2F800]`}
+          >
+            Workout Library
+          </p>
+
+          <h1 className="max-w-125 text-6xl font-bold uppercase text-white sm:text-5xl">
+            Train with intent. Log every set.
+          </h1>
+
+          <p
+            className={`${inter.className} max-w-105 text-[16px] tracking-wider leading-relaxed text-[#9CA3AF] sm:text-xs`}
+          >
+            FitLog is a dark, no-nonsense gym companion: pick a lift, lock it
+            into todays plan, and watch the weeks work add up.
+          </p>
+
+          <button
+            className={`${inter.className} rounded-md bg-[#C2F800] px-6 py-3 text-xs font-bold uppercase text-black`}
+          >
+            Browse Workouts
+          </button>
+        </div>
+
+        {/* Right Image */}
+        <div>
+          <Image src={BannerImage} alt="Banner" />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Banner;
